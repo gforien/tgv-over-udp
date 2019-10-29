20fbd5a70d0964e65f9de6e607fe3afd9189a5fa
Gabriel Forien <gforien@gmail.com>
2019-10-29 19:43:39 +0100

    :wrench: work in progress

--- src/com/ebgf/TGVOverUDP/Serveur.java
+++ src/com/ebgf/TGVOverUDP/Serveur.java
@@ -6,11 +6,26 @@ import java.io.*;
 public abstract class Serveur implements Runnable {
 
     public static final int MAXBUFFSIZE = 8192;
-    public static final int NBYTESEQ = 6;
+    public static final int NBYTESEQ    = 6;
+
+    public static final String RESET  = "\033[0m";
+    public static final String NOIR   = "\033[1;30m";
+    public static final String ROUGE  = "\033[1;31m";
+    public static final String VERT   = "\033[1;32m";
+    public static final String JAUNE  = "\033[1;33m";
+    public static final String BLEU   = "\033[1;34m";
+    public static final String VIOLET = "\033[1;35m";
+    public static final String CYAN   = "\033[1;36m";
+    public static final String BLANC  = "\033[1;37m";
+
+    public int debugLevel = 1;
+    public String debugColog = BLANC;
+    public long retard;
 
     // attributs du serveur : définis par le constructeur
     protected int port;
     protected DatagramSocket socket;
+    protected int seq;
 
     // attributs du clients : définis par initClientApresRecu()
     protected int portClient;
@@ -22,11 +37,10 @@ public abstract class Serveur implements Runnable {
     protected DatagramPacket packetRecu;
     protected DatagramPacket packetEnvoi;
 
-    // fonctionnalités supplémentaires : ACK, n° seq, envoi de fichier
+    // fonctionnalités supplémentaires : à définir dans les classes filles qui veulent l'implémenter
     protected int dernierACKRecu;
-    protected int seq;
-    protected boolean fichierEnCours;
-    protected String nomFichier;
+    //protected boolean fichierEnCours;
+    //protected String nomFichier;
     protected BufferedInputStream fluxFichier;
 
 
@@ -34,7 +48,7 @@ public abstract class Serveur implements Runnable {
     /******************************************************************************/
     //                   Méthodes à redéfinir selon le comportement voulu
     //
-                        public abstract void initConnection();
+                        public abstract void initConnection() throws Exception;
                         public abstract void run();
     //
     /******************************************************************************/
@@ -42,91 +56,125 @@ public abstract class Serveur implements Runnable {
 
 
     public Serveur(int port) throws IOException {
-        this.port = port;
-        this.socket = new DatagramSocket(this.port);
+        log("port = "+port);
+        this.port       = port;
+        this.socket     = new DatagramSocket(this.port);
+        this.seq        = 1;
+        log("");
+    }
+
+    public void log(int level, String msg, String couleur) {
+        if(level < this.debugLevel)
+            return;
+        long start = System.currentTimeMillis();
+
+        StringBuffer sb = new StringBuffer(100);
+        String t = Thread.currentThread().getName();
+        String m = Thread.currentThread().getStackTrace()[3].getMethodName();
+
+        sb.append("[" + t + "] " + m + "()");
+        for (int i=sb.length(); i<35; i++) sb.append(" ");
+
+        System.out.println( couleur + sb + msg + RESET);
+        this.retard += System.currentTimeMillis()-start;
+    }
+    public void log(int level, String msg) {
+        if (level>1)
+            log(level, msg, ROUGE);
+        else
+            log(level, msg, this.debugColog);
+    }
+    public void log(String msg) {
+        log(1, msg, this.debugColog);
+    }
+    public void log(String msg, String couleur) {
+        log(1, msg, couleur);
     }
 
 
     protected void initClientApresRecu() throws IOException {
+        log("");
         this.addrClient = this.packetRecu.getAddress();
         this.portClient = this.packetRecu.getPort();
-        this.packetEnvoi.setAddress(this.addrClient);
-        this.packetEnvoi.setPort(this.portClient);
+        log("client "+this.addrClient.toString()+":"+this.portClient);
+
+        this.bufferEnvoi = new byte[2000];
+        this.packetEnvoi = new DatagramPacket(bufferEnvoi, bufferEnvoi.length, this.addrClient, this.portClient);
+        log("");
     }
-
-
-
 
 
     // on se prépare à recevoir n bytes dans bufferRecu
     protected void initRecu(int tailleByte) {
+        log("tailleByte = "+tailleByte);
         this.bufferRecu = new byte[tailleByte];
         this.packetRecu = new DatagramPacket(this.bufferRecu, this.bufferRecu.length);
+        log("");
     }
 
-    protected void initEnvoi(boolean avecNumSeq, boolean envoiFichier, String messageOuNomFichier) throws FileNotFoundException, IOException{
-
-        // (1) on définit la taille du message
-        int tailleByte;
-        if (envoiFichier) {
-            tailleByte = (avecNumSeq)? MAXBUFFSIZE-NBYTESEQ: MAXBUFFSIZE;
-        } else {
-            tailleByte = (avecNumSeq)? messageOuNomFichier.length()+NBYTESEQ: messageOuNomFichier.length();
-        }
-        this.bufferEnvoi = new byte[tailleByte];
+    protected void initEnvoiChaine(String message) {
+        log("msg = "+message);
+        this.bufferEnvoi = new byte[message.length()];
         this.packetEnvoi.setLength(this.bufferEnvoi.length);
 
-        // (2) on remplit les NBYTESEQ premiers bytes avec seq, si besoin
-        int offset = 0;
-        if (avecNumSeq) {
-            /*int puissance = 0;
-            int seq2 = this.seq;
-            while ((seq2/=10) != 0) puissance++;*/
-            int seq2 = 1234;
-            for (int i=NBYTESEQ-1; i>=0; i--) {
-                bufferEnvoi[i] = (byte)(seq2 %10);
-                seq2 /= 10;
-                System.out.println(bufferEnvoi[i]);
-            }
-            offset = NBYTESEQ-1;
+        for (int i=0; i<this.bufferEnvoi.length; i++) {
+            bufferEnvoi[i] = (byte)(message.charAt(i));
         }
-
-        // (3) on charge le contenu
-        if (envoiFichier && this.fichierEnCours) {
-            fluxFichier.read(bufferEnvoi, offset, bufferEnvoi.length-offset);
-        } else {
-            for (int i=0; i<messageOuNomFichier.length(); i++) {
-                bufferEnvoi[offset+i] = (byte)(messageOuNomFichier.charAt(i));
-            }
-        }
+        log("");
     }
 
+    // attention, on doit avoir défini fluxfichier !
+    protected int initEnvoiFichier() throws IOException{
+        log("");
+        this.bufferEnvoi = new byte[MAXBUFFSIZE-NBYTESEQ];
+        this.packetEnvoi.setLength(this.bufferEnvoi.length);
+
+        int offset = NBYTESEQ-1;
+        int seq2 = this.seq;
+        for (int i=offset; i>=0; i--) {
+            this.bufferEnvoi[i] = (byte)(seq2 %10);
+            seq2 /= 10;
+            log(1, "initEnvoiFichier(): "+String.valueOf((char)this.bufferEnvoi[i]), VERT);
+        }
+
+        log("");
+        return fluxFichier.read(this.bufferEnvoi, offset, this.bufferEnvoi.length-offset);
+    }
 
 
     protected void recoitBloquant() throws IOException {
+        log("");
         this.socket.receive(this.packetRecu);
+        log("");
     }
 
     protected void envoiBloquant() throws IOException {
+        log("");
         this.socket.send(this.packetEnvoi);
+        log("");
     }
+
+
 
     // on compare bufferRecu au message qu'on était censés recevoir: s'ils ne sont pas identiques on lève une exception
     protected void verifieRecu(String messageAttendu) throws Exception {
+        log("messageAttendu = "+messageAttendu);
         String messageRecu = new String(this.bufferRecu, "UTF-8");
         if (!messageAttendu.equals(messageRecu)) {
             throw new Exception("verifieRecu(): message recu '"+messageRecu+"' au lieu du message attendu '"+messageAttendu+"'");
         }
+        log("");
     }
 
     // surcharge
     protected void verifieRecu(int ackAVerifier) throws Exception {
+        log("ackAVerifier = "+ackAVerifier);
         char[] seqChar = new char[NBYTESEQ];
         for (int i=NBYTESEQ-1; i>=0; i--) {
             seqChar[i] = (char)(ackAVerifier %10);
             ackAVerifier /= 10;
-            System.out.println(seqChar[i]);
         }
         verifieRecu("ACK"+new String(seqChar));
+        log("");
     }
 }
--- src/com/ebgf/TGVOverUDP/Test.java
+++ src/com/ebgf/TGVOverUDP/Test.java
@@ -7,10 +7,11 @@ public class Test {
     public static void main(String[] args) throws IOException {
 
         try {
-            Serveur s = new Test.ThreadMere(Integer.parseInt(args[0]));
 
+            int port = Integer.parseInt(args[0]);
 
-
+            Thread t = new Thread(new Test.ThreadMere(port), "MERE :"+String.valueOf(port));
+            t.start();
 
         } catch (NumberFormatException err) {
             System.out.println("usage: ./server server_port");
@@ -25,16 +26,48 @@ public class Test {
 //                    Définition du comportement des différents threads
 /******************************************************************************************/
     public static class ThreadMere extends Serveur {
+
+        public int portDedie = 1234;
+
         public ThreadMere(int port) throws IOException {
             super(port);
+            this.debugLevel = 1;
         }
 
         @Override
-        public void initConnection() {}
+        public void initConnection() throws Exception {
+            // three-way handshake
+            initRecu(3);                // on reçoit SYN ou ACK -> 3 caractères
+            recoitBloquant();
+            verifieRecu("SYN");
+
+            initClientApresRecu();      // après avoir reçu un message on a bien les infos du client
+
+            /*bufferEnvoi = new byte[]{'S', 'Y', 'N', '-', 'A', 'C', 'K', '3', '0', '0', '0'};
+            packetEnvoi = new DatagramPacket(bufferEnvoi, bufferEnvoi.length);*/
+
+            initEnvoiChaine("SYN-ACK"+String.valueOf(portDedie));
+            this.portDedie++;
+            envoiBloquant();
+            System.out.println("SYN-ACK envoyé");
+
+            recoitBloquant();
+            verifieRecu("ACK");
+        }
 
         @Override
-        public void run(){
-            System.out.println("Nouveau ThreadMere !");
+        public void run() {
+            try {
+                while(true) {
+                    initConnection();
+                    System.out.println(this.retard);
+
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
         }
     }
 
@@ -45,15 +78,33 @@ public class Test {
         }
 
         @Override
-        public void initConnection() {}
+        public void initConnection() throws IOException, FileNotFoundException {
+
+            // on reçoit un nom de fichier d'une taille quelconque
+            initRecu(1000);
+            recoitBloquant();
+            initClientApresRecu();
+
+            System.out.println("Fichier requis: "+new String(bufferRecu, "UTF-8"));
+            this.fluxFichier = new BufferedInputStream(new FileInputStream(new String(bufferRecu, "UTF-8")));
+        }
 
         @Override
-        public void run(){}
+        public void run() {
+            try {
+                initConnection();
+
+                while(initEnvoiFichier() != -1) {
+                    envoiBloquant();
+                }
+
+                initEnvoiChaine("FIN");
+                envoiBloquant();
+            } catch (Exception e) {
+                return;
+            }
+        }
     }
-
-
-
-
 
 }
 
@@ -107,21 +158,5 @@ b & 0xFF
 
                     while( bis.read(buffer) != -1 ) {
                 bos.write(buffer);
-    }
-
-    private void threeWayHandshake() throws IOException {
-        initRecu(3);                // on reçoit SYN ou ACK -> 3 caractères
-        recoitBloquant();
-        verifieRecu("SYN");
-
-        initClientApresRecu();      // après avoir reçu un message on a bien les infos du client
-
-        bufferEnvoi = new byte[]{'S', 'Y', 'N', '-', 'A', 'C', 'K', '3', '0', '0', '0'};
-        packetEnvoi = new DatagramPacket(bufferEnvoi, bufferEnvoi.length);
-        envoiBloquant();
-        System.out.println("SYN-ACK3000 envoyé");
-
-        recoitBloquant();
-        verifieRecu("ACK");
     }
 */
