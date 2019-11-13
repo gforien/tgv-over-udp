0a61e9de6f470e0ce26bacdd9ad1efcf6e86087c
Gabriel Forien <gforien@gmail.com>
2019-11-13 16:11:42 +0100

    MAXBUFFSIZE à 65000

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#set -x
+
 # 1 - tout compiler
 echo "make -f src/Makefile"
 make -f src/Makefile
@@ -10,21 +12,22 @@ port=2000
 if [[ $(pgrep java ) ]]; then
     echo "Serveur déjà lancé"
 else
-    echo "java -cp bin com.ebgf.TGVOverUDP.Test $ip $port"
-    $(java -cp bin com.ebgf.TGVOverUDP.Test $ip $port) & > /dev/null 2>&1
+    echo "Lancer le serveur dans un terminal séparé"
+    #echo "java -cp bin com.ebgf.TGVOverUDP.Test $ip $port"
+    #$(java -cp bin com.ebgf.TGVOverUDP.Test $ip $port) & > /dev/null 2>&1
 fi
 
 # 3 - Lancer les clients
-echo "./client1 $ip $port 5Mo"
-{ time ./bin/client1 $ip $port 5Mo 0; } 2>temp
+echo "./client1 $ip $port 25Mo"
+{ time ./bin/client1 $ip $port 25Mo 0; } 2>temp
 t=$(cat temp | grep real | cut -f 2 | cut -d m -f 2 | cut -d s -f 1)
-debit=$(echo "scale=2; 8*5 / $t" | bc -l)
+debit=$(echo "scale=2; 8*25 / $t" | bc -l)
 echo -e "temps = $t s\t\tdebit = $debit Mb/s"
 
-echo "./client2 $ip $port 5Mo"
-{ time ./bin/client2 $ip $port 5Mo 0; } 2>temp
+echo "./client2 $ip $port 25Mo"
+{ time ./bin/client2 $ip $port 25Mo 0; } 2>temp
 t=$(cat temp | grep real | cut -f 2 | cut -d m -f 2 | cut -d s -f 1)
-debit=$(echo "scale=2; 8*5 / $t" | bc -l)
+debit=$(echo "scale=2; 8*25 / $t" | bc -l)
 echo -e "temps = $t s\t\tdebit = $debit Mb/s"
 
 
